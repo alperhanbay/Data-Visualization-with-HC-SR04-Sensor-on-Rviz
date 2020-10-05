4bcb6f7570420017bf6ea2fe8c9bfa7e306ed807
Alperhan Bay <alperhanbay@gmail.com>
2020-10-05 14:14:56 +0300

    Data-Visualization-with-HC-SR04-Sensor-on-Rviz
    Updated.

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/alperhan/sonar_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/alperhan/sonar_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/alperhan/gems/bin:/usr/local/cuda/bin:/home/alperhan/bin:/home/alperhan/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PWD="/home/alperhan/sonar_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/alperhan/sonar_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/alperhan/sonar_ws/src:$ROS_PACKAGE_PATH"
